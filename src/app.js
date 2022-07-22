2db265603862d222cd1d65609117d3e3d8aca28a
AbdulKhalekG <adnan_akg_1401@hotmail.com>
2022-07-22 00:17:56 -0400

    modificaciones

--- src/app.js
+++ src/app.js
@@ -1,12 +1,75 @@
 const express = require ('express')
+const app = express()
 const session = require ('express-session')
 const flash = require('express-flash')
 const passport = require('passport')
 const {Strategy} =require('passport-local')
 const { LocalStrategy } = require('./strategies')
-const app = express()
+
+const http = require("http");
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+let users = [];
+
+  const messages = {
+    general: [],
+    random: [],
+    jokes: [],
+    javascript: []
+  };
 
 
+
+io.on('connection', socket => {
+  socket.on("join server", (username) => {
+    const user = {
+      username,
+      id:socket.id,
+    };
+    users.push(user);
+    io.emit("new user", users);
+  });
+
+  socket.on("join room", (roomName, cb) => {
+    socket.join(roomName);
+    cb(messages[roomName]);
+  });
+
+
+  socket.on("send message", ({ content, to, sender, chatName, isChannel }) => {
+    if(isChannel) {
+      const payload = {
+        content,
+        chatName,
+        sender,
+
+      };
+      socket.to(to).emit("new message", payload);
+    }else {
+      const payload = {
+        content,
+        chatName: sender,
+        sender
+      };
+      socket.to(to).emit("new message", payload);
+    }
+    if (messages[chatName]) {
+      messages[chatName].push({
+        sender,
+        content
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter(u => u.id !== socket.id);
+    io.emit("new user", users);
+  });
+});
+
+//
 //middlewares
 app.use(session({
     secret:'jajaja',
